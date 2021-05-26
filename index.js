b6ac45c451670edd6338c34b5c83b094e3b62e5a
kendra Hood <my.github.email@example.com>
2021-05-26 14:11:04 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+const cats = [ "Milo", "Otis", "Garfield"];
+    //cats;
+
+function destructivelyAppendCat() {
+    cats.push( "Ralph" );
+}
+
+function destructivelyPrependCat() {
+    cats.unshift( "Bob" );
+}
+
+function destructivelyRemoveLastCat() {
+    cats.splice(2);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.splice(0,1);
+}
+
+function appendCat(){
+    const copyCats = [...cats, "Broom"];
+    return copyCats;
+}
+
+function prependCat(){
+    const copyCats = ["Milo", "Otis", "Garfield"]
+    copyCats.unshift("Arnold");
+    return copyCats; 
+}
+
+function removeLastCat(){
+    const copyCats = ["Milo", "Otis", "Garfield"]
+    copyCats.pop();
+    return copyCats; 
+}
+
+function removeFirstCat(){
+    const copyCats = ["Milo", "Otis", "Garfield"]
+    copyCats.shift()
+    return copyCats;
+}
+
+        
+      
+
+
+
+
